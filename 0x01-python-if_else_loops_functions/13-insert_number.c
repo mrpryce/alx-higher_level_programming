b34b879acc181ac6ca9dfa07340772c06e324be5
Bright Otu <bionethe69@gmail.com>
2022-11-29 20:00:33 -0800

    fixed for 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,42 +1,45 @@
 #include "lists.h"
 
 /**
- * insert_node - inserts a number into a sorted singly linked list.
- * @head: list head
- * @number: number to store in the new node
- * Return: pointer to the new node
+ * insert_node - Entry Point
+ *
+ * @head: the list passed in
+ * @number: value of the new node
+ *
+ * Description: Inserts a node in an ordered linked list
+ *
+ * Return:  The address of the new node or NULL if failure
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *runner;
-        listint_t *new;
+listint_t *newNode, *firstNode;
 
-        runner = *head;
+firstNode = *head;
 
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = number;
+newNode = malloc(sizeof(listint_t));
+if (newNode == NULL)
+return (NULL);
+newNode->n = number;
 
-        if (*head == NULL || (*head)->n > number)
-        {
-                new->next = *head;
-                *head = new;
-                return (new);
-        }
+if (*head == NULL || (*head)->n > number)
+{
+newNode->next = *head;
+*head = newNode;
+return (newNode);
+}
 
-        while (runner->next != NULL)
-        {
-                if ((runner->next)->n >= number)
-                {
-                        new->next = runner->next;
-                        runner->next = new;
-                        return (new);
-                }
-                runner = runner->next;
-        }
-
-        new->next = NULL;
-        runner->next = new;
-        return (new);
+while (firstNode->next != NULL)
+{
+if ((firstNode->next)->n >= number)
+{
+newNode->next = firstNode->next;
+firstNode->next = newNode;
+return (newNode);
+}
+firstNode = firstNode->next;
+}
+firstNode->next = newNode;
+newNode->next = NULL;
+return (newNode);
 }
